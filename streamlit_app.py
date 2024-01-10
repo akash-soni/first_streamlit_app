6ea16772600a377b4774cb7b57293eb84b10316a
akash-soni <37892453+akash-soni@users.noreply.github.com>
2024-01-10 14:41:56 +0530

    multiselect added

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,8 @@ streamlit.text('Kale, Spinach & Rocket smoothie')
 streamlit.text('hard boiled, free-range eggs')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+streamlit.multiselect("Pick up some fruits:",list(my_fruit_list.index))
+
 streamlit.dataframe(my_fruit_list)
