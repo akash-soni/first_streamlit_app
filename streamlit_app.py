fdc0878d66e0e4e0a10df0c5af4e4afa034f8b81
akash-soni <37892453+akash-soni@users.noreply.github.com>
2024-01-17 15:29:03 +0530

    added insert line in app

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,5 @@ streamlit.dataframe(my_data_rows)
 # Add second input
 second_fruit_choice = streamlit.text_input('What fruit would you like to add?','')
 streamlit.write('Thanks for adding ', second_fruit_choice)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
